9b9b8ff16d9d3c21403ffec6272668f25c29e646
moby812 <Moby812@gmail.com>
2022-12-16 10:54:22 +0400

    Реализовал screenshot

--- src/main/java/Options.java
+++ src/main/java/Options.java
@@ -20,7 +20,7 @@ public class Options {
         options.addArguments("--disable-extensions");
 
         WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(120));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(600));
 //        driver.manage().window().maximize();
         return driver;
     }
