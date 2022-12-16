90b8693c3ea722d0edd3cb063d0520fb473ee6d6
moby812 <Moby812@gmail.com>
2022-12-16 10:13:25 +0400

    Реализовал screenshot

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,6 +1,8 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
 
 abstract public class BaseTest {
@@ -14,6 +16,7 @@ abstract public class BaseTest {
         Configuration.driverManagerEnabled = true;
         Configuration.browserSize = "1920x1080";
         Configuration.headless = true;     //будет ли виден браузер
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
 
